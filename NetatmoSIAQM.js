9cf0a6dcf0e039705b7e074e7b4cd03148a60336
Fredrik Johansson <fredrik@johanssonrobotics.com>
2022-06-27 22:53:59 +0200

    Update NetatmoSIAQM.js
    Support for multiple widget sizes

--- NetatmoSIAQM.js
+++ NetatmoSIAQM.js
@@ -12,7 +12,7 @@ client_secret:CoCNPDjo9XXXXXXYYYyEJWEwwzXXXXXN16d1
 username:yournetatmoaccount@email.com
 password:secretpassword
 4) Paste the string in the params variable below.
-5) On your home screen, add the new Scriptable Widget as a medium sized widget with the script.
+5) On your home screen, add the new Scriptable Widget as a (medium sized preferred) widget with the script.
 
 Provided for free, MIT, as-is, by fredrikdev 2022. Inspired by https://github.com/olf/scriptable-netatmo-widget
 */
@@ -111,44 +111,166 @@ s.layoutVertically()
 let f = new DateFormatter()
 f.useMediumDateStyle()
 f.useShortTimeStyle()
+// layout data on widget
 
+// setup
+let padding = 6*Device.screenScale()
+let paddingLine = 1*Device.screenScale()
+let widgetFamily = config.widgetFamily || "medium";
+
+let f = new DateFormatter()
+f.useMediumDateStyle()
+f.useShortTimeStyle()
 let fm16 = Font.mediumSystemFont(14)
-
-let r1 = row(s, [ 
-{ t1: `${d.station_name}`, f1: fm16 },
-{ t1: `${f.string(new Date(dd.time_utc * 1000))}` } ]
-)
-r1.setPadding(padding, padding, 0, padding)
-s.addSpacer()
-
-// row 2
+let fl16 = Font.lightSystemFont(14)
 let fm40 = Font.mediumRoundedSystemFont(40)
 let fl40 = Font.lightRoundedSystemFont(40)
 
-let r2 = row(s, [
-{ t1: `${dd.Temperature}`, f1: fm40, t2: `°`, f2: fl40 } ], [
-{ t1: `⤒ ${dd.max_temp || dd.Temperature}°`, vertical: 2, padt: 6 },
-{ t1: `⤓ ${dd.min_temp || dd.Temperature}°`, padb: 6 } ]
-)
-r2.setPadding(0, padding, 0, padding)
-s.addSpacer()
+// helper
+function row(p) {
+  let r = p.addStack()
+  let vertical = 0
+  for (let x = 1; x < arguments.length; x++) {
+    let i = arguments[x]
+    let c = r.addStack()
+    for (let y = 0; y < i.length; y++) {
+      let s = i[y]
+      if (s.vertical) {
+        c.layoutVertically()
+        c.centerAlignContent()
+        vertical = s.vertical
+      } else {
+        c.centerAlignContent()
+      }
+      let tc = c.addStack(), t1 = null, t2 = null
+      tc.setPadding(s.padt || 0, 0, s.padb || 0, 0)
+      t1 = tc.addText(s.t1)
+      t1.textColor = Color.white()
+      t1.font = s.f1|| fl16
+      t1.lineLimit = 1
+      if (s.t2) {
+        t2 = tc.addText(s.t2)
+        t2.textColor = Color.white()
+        t2.font = s.f2 || fl16
+        t2.lineLimit = 1
+      }
+      
+      if (vertical) {
+        if (y == i.length - 1) {
+        } else {
+          c.addSpacer(vertical == 99 ? null : vertical)
+        }
+      } else {
+        if (y == i.length - 1) {
+          if (t1) t1.rightAlignText()
+          if (t2) t2.rightAlignText()
+        } else {
+          c.addSpacer()
+        }
+      }
+    }  
+    if (x != arguments.length - 1) {
+       r.addSpacer()
+    }
+  }
+  return r
+}
 
-// row 3
-let r3 = row(s, [
-{ t1: `HUMIDITY`, vertical: paddingLine },
-{ t1: `${dd.Humidity}`, f1: fm16, t2: ` %`}
-], [
-{ t1: `CO₂`, vertical: paddingLine },
-{ t1: `${dd.CO2}`, f1: fm16, t2: ` ppm`}
-], [
-{ t1: `NOISE`, vertical: paddingLine },
-{ t1: `${dd.Noise}`, f1: fm16, t2: ` dB`}
-], [
-{ t1: `PRESSURE`, vertical: paddingLine },
-{ t1: `${dd.AbsolutePressure}`, f1: fm16, t2: ` mbar` }
-])
-r3.setPadding(padding/2, padding, padding/2, padding)
-r3.backgroundColor = g.colors[0]
+// layout
+let w = new ListWidget()
+w.setPadding(0, 0, 0, 0)
+w.minimumScaleFactor = 0.6
+
+let g = new LinearGradient()
+g.colors = [new Color("64C5E8"),new Color("2F83B9")]
+g.locations = [0,1.5]
+w.backgroundGradient = g
+
+let s = w.addStack()
+s.layoutVertically()
+
+if (widgetFamily == "small") {
+  padding = 6*Device.screenScale()
+  f.useNoDateStyle()
+  fm16 = Font.mediumSystemFont(12)
+  fl16 = Font.lightSystemFont(12)
+  fm40 = Font.mediumRoundedSystemFont(34)
+  fl40 = Font.lightRoundedSystemFont(34)
+  
+  // row 1
+  let r1 = row(s, [ 
+  { t1: `${d.station_name}`, f1: fm16 },
+  { t1: `${f.string(new Date(dd.time_utc * 1000))}` } ]
+  )
+  r1.setPadding(padding, padding, 0, padding)
+  
+  // row 2  
+  let r2 = row(s, [
+  { t1: `${Math.trunc(dd.Temperature)}`, f1: fm40, t2: `°`, f2: fl40 } ], [
+  { t1: `⤒ ${Math.trunc(dd.max_temp || dd.Temperature)}°`, vertical: 1, padt: 6 },
+  { t1: `⤓ ${Math.trunc(dd.min_temp || dd.Temperature)}°`, padb: 6 } ]
+  )
+  r2.setPadding(0, padding, paddingLine, padding)
+  
+  // rows 3-6
+  let sN = s.addStack()
+  sN.layoutVertically()
+  sN.setPadding(paddingLine*2, padding, padding, padding)
+  sN.backgroundColor = g.colors[0]
+  
+  row(sN, [
+  { t1: `HUMIDITY` },
+  { t1: `${dd.Humidity}`, f1: fm16, t2: ` %`}
+  ]).setPadding(0, 0, paddingLine, 0)
+  
+  row(sN, [
+  { t1: `CO₂` },
+  { t1: `${dd.CO2}`, f1: fm16, t2: ` ppm`}
+  ]).setPadding(0, 0, paddingLine, 0)
+  
+  row(sN, [
+  { t1: `NOISE` },
+  { t1: `${dd.Noise}`, f1: fm16, t2: ` dB`}
+  ]).setPadding(0, 0, paddingLine, 0)
+  
+  row(sN, [
+  { t1: `PRESSURE` },
+  { t1: `${Math.trunc(dd.AbsolutePressure)}`, f1: fm16, t2: ` mb` }]).setPadding(0, 0, 0, 0)
+} else {
+  // row 1
+  let r1 = row(s, [ 
+  { t1: `${d.station_name}`, f1: fm16 },
+  { t1: `${f.string(new Date(dd.time_utc * 1000))}` } ]
+  )
+  r1.setPadding(padding, padding, 0, padding)
+  s.addSpacer()
+  
+  // row 2
+  let r2 = row(s, [
+  { t1: `${dd.Temperature.toFixed(1)}`, f1: fm40, t2: `°`, f2: fl40 } ], [
+  { t1: `⤒ ${(dd.max_temp || dd.Temperature).toFixed(1)}°`, vertical: 2, padt: 6 },
+  { t1: `⤓ ${(dd.min_temp || dd.Temperature).toFixed(1)}°`, padb: 6 } ]
+  )
+  r2.setPadding(0, padding, 0, padding)
+  s.addSpacer()
+  
+  // row 3
+  let r3 = row(s, [
+  { t1: `HUMIDITY`, vertical: paddingLine },
+  { t1: `${dd.Humidity}`, f1: fm16, t2: ` %`}
+  ], [
+  { t1: `CO₂`, vertical: paddingLine },
+  { t1: `${dd.CO2}`, f1: fm16, t2: ` ppm`}
+  ], [
+  { t1: `NOISE`, vertical: paddingLine },
+  { t1: `${dd.Noise}`, f1: fm16, t2: ` dB`}
+  ], [
+  { t1: `PRESSURE`, vertical: paddingLine },
+  { t1: `${Math.trunc(dd.AbsolutePressure)}`, f1: fm16, t2: ` mbar` }
+  ])
+  r3.setPadding(padding/2, padding, padding/2, padding)
+  r3.backgroundColor = g.colors[0]
+}
 
 // display
 if (config.runsInApp) {
