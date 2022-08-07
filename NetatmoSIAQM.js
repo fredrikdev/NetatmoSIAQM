59ff534c398334de59efbcf0d0d692fe8a50d3b9
Fredrik Johansson <fredrik@johanssonrobotics.com>
2022-08-07 15:33:57 +0200

    Update NetatmoSIAQM.js

--- NetatmoSIAQM.js
+++ NetatmoSIAQM.js
@@ -5,9 +5,9 @@ https://github.com/fredrikdev/NetatmoSIAQM
 Scriptable Widget for the Netatmo Smart Indoor Air Quality Monitor (usually interfaced with by the iOS app Netatmo Home Coach) using the Netatmo Aircare API.
 
 1) Login to your Netatmo Account at https://dev.netatmo.com/, and create a new app: https://dev.netatmo.com/apps/createanapp (yes, unfortunately you'll need to do this, but it's pretty swift)
-2) Fill in the mandatory fields, save, and continue by setting the Redirect URI field to "https://noop". Save again, and take note of the Client Id and Client Secret fields.
-3a) Using your browser, construct and browse to the URL: https://api.netatmo.com/oauth2/authorize?client_id=xxx&scope=read_homecoach&redirect_uri=https://noop
-3b) ...After authenticating with Netatmo, you'll be redirected to a non-existing URL (e.g. https://noop/?code=yyy). Copy the value (yyy) from the &code= parameter.
+2) Fill in the mandatory fields, save, and continue by setting the Redirect URI field to "https://noop". Save again, and take note of the Client Id (aaa) and Client Secret (bbb) fields.
+3a) Using your browser, construct and browse to the URL: https://api.netatmo.com/oauth2/authorize?client_id=aaa&scope=read_homecoach&redirect_uri=https://noop
+3b) ...After authenticating with Netatmo, you'll be redirected to a non-existing URL (e.g. https://noop/?code=ccc). Copy the value (ccc) from the &code= parameter.
 
 4) Create a new script in the iOS Scriptable app, add a name, color, glyph, and paste this script.
 5) Edit the script below and set the 'params' variable with your own Client Id, Client Secret and value from the &code= parameter.
@@ -20,7 +20,7 @@ Provided for free, MIT, as-is, by fredrikdev 2022. Inspired by https://github.co
 let params = `
 client_id:aaa
 client_secret:bbb
-code:yyy
+code:ccc
 `
 
 function param(name) {
